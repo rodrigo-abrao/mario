91476a4a88fd635c2ea4e2d52155afc1ef1075e7
Rodrigo Abrão de Barros <rod.abraob123@gmail.com>
2024-08-17 00:45:14 -0300

    jogo finalizado!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+
 
 const jump = () => {
     mario.classList.add('jump')
@@ -9,5 +11,27 @@ const jump = () => {
 
    }, 500)
 }
+
+const loop = setInterval(() => {
+
+    const pipeposition = pipe.offsetLeft;
+    const marioposition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    
+    if (pipeposition <= 120 && pipeposition > 0 && marioposition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipeposition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom= `${marioposition}px`;
+        mario.src = '/images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+
+    }
+
+}, 10)
 
 document.addEventListener('keydown', jump);
